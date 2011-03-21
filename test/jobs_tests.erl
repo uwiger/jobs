2c705269531400df3985a08dc0b8002ef5662706
Ulf Wiger <ulf.wiger@erlang-solutions.com>
2011-03-21 23:51:04 +0100

    try changing paths in slave node

--- test/jobs_tests.erl
+++ test/jobs_tests.erl
@@ -64,11 +64,7 @@ start_slave(Name) ->
 	_ ->
 	    ok
     end,
-    io:fwrite(user, "lib_dir(jobs) -> ~p~n", [code:lib_dir(jobs)]),
-    Path = code:lib_dir(jobs) ++ "/ebin",
-    Path1 = filename:dirname(code:which(jobs)),
-    {ok, Node} = slave:start(host(), Name, "-pa " ++ Path1 ++
-			    " -pz " ++ Path),
+    {ok, Node} = slave:start(host(), Name, "-pa . -pz ../ebin"),
     io:fwrite(user, "Slave node: ~p~n", [Node]),
     Node.
 
